2674fd5073afb7065c2cfae18dc625ce21257ae6
raviteja-nagishetti <ravitejanagisetti@gmail.com>
2019-12-20 14:54:26 +0530

    Adding Project Files

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('frontend'))
+app.get('/', (req, res) => res.sendFile(__dirname+'/frontend/html/home.html'))
+//res.sendFile
+//res.json
+app.get('/login', (req, res) => res.send('Login'))
+app.get('/register', (req, res) => res.send('Register'))
+
+var student = {"name" : "Raviteja",
+               "college" : "VNRVJIET",
+               "regno": "112323232"}
+
+app.get('/data', (req, res) => res.json(student))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
